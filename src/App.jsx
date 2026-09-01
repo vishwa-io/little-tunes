1f5a218c8c0582c3de92714b654517f725057373
vishwa-io <233765120+vishwa-io@users.noreply.github.com>
2026-09-01 13:47:22 +0530

    added play logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css"
 import cover from "./assets/cover.png"
+import song1 from "./assets/music/song-1.mp3"
+import { useRef } from "react"
 
 function App() {
+  const audioRef = useRef(null)
+
+  function playSong() {
+    if (audioRef.current.paused) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+    }
+  }
   return (
     <main className="page">
       <div className="player">
@@ -12,9 +23,10 @@ function App() {
           <input type="range" />
           <span>0:00</span>
         </div>
+        <audio className="audio" src={song1} ref={audioRef}></audio>
         <div className="controls">
           <button className="previous">previous</button>
-          <button className="play">play</button>
+          <button className="play" onClick={playSong}>play</button>
           <button className="next">next</button>
         </div>
       </div>
